59e26577da7ad395a8ff4bede521c0e31757d740
Francisco Joray <franjoray@gmail.com>
2026-09-20 23:10:50 -0300

    update Rocq version in headers

--- src/KSOp.v
+++ src/KSOp.v
@@ -3,7 +3,7 @@
  * Formalizing Domains, Ultrametric Spaces and Semantics of Programming Languages *
  * Nick Benton, Lars Birkedal, Andrew Kennedy and Carsten Varming                 *
  * Jan 2012                                                                       *
- * Build with Coq 8.3pl2 plus SSREFLECT                                           *
+ * Build with Rocq 9.0.1 plus mathcomp SSREFLECT                                  *
  **********************************************************************************)
 
 (* Operational semantics of the kitchen sink language *)
